e1f69cc81da55a6648507999c98267b2d85db388
Varun Jayaram <51790598+Varun-2712@users.noreply.github.com>
2024-03-17 21:01:09 +0530

    Add files via upload
    First Commit

---
+++ saga-choreography/choreography-common/src/main/java/com/vinsguru/Main.java
@@ -0,0 +1,10 @@
+package com.vinsguru;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
